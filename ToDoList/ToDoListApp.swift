91d4e8526b96582f63f882b9257372b178188eb9
SherryFang1207 <zfang1207@gmail.com>
2023-12-01 11:51:30 -0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 房子祺 on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
